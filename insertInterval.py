b7a7a511d89a8ac0b7f635f49a1e7de8380c51a6
Marc <mecquilatan@gmail.com>
2021-08-09 16:28:36 -0400

    added insertInterval.py

---
+++ insertInterval.py
@@ -0,0 +1,25 @@
+def insert(intervals, newInterval):
+    """
+    :type intervals: List[List[int]]
+    :type newInterval: List[int]
+    :rtype: List[List[int]]
+    """
+
+    res = []
+
+    for i in range(len(intervals)):
+        if newInterval[1] < intervals[i][0]:
+            res.append(newInterval)
+            return res + intervals[i:]
+        elif newInterval[0] > intervals[i][1]:
+            res.append(intervals[i])
+        else:
+            newInterval = [min(newInterval[0], intervals[i]
+                               [0]), max(newInterval[1], intervals[i][1])]
+
+    res.append(newInterval)
+
+    return res
+
+
+print(insert([[1, 3], [6, 9]], [2, 5]))
