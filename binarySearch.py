d6c43ea400a464981e93b4de741093de83b57778
Marc <mecquilatan@gmail.com>
2021-10-05 15:01:18 -0400

    Added Binary search problem from leetcode

---
+++ binarySearch.py
@@ -0,0 +1,29 @@
+def binarySearch(nums, target):
+    """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+    R = len(nums) - 1
+    L = 0
+    if len(nums) == 1 and nums[0] == target:
+        return 0
+    while (R-L) > 2:
+
+        mid = (R+L)//2
+        if nums[mid] == target:
+            return mid
+        elif nums[R] == target:
+            return R
+        elif nums[L] == target:
+            return L
+        elif nums[mid] < target:
+            L = mid
+        elif nums[mid] > target:
+            R = mid
+
+    return -1
+
+
+print(binarySearch([2, 5], 5))
